117e87a7f194be542fb066deb99b4a2ec5ef1dbb
Alok Chakervarty <Alok.Chakervarty@smawins.com>
2026-07-21 11:11:55 +0530

    making swagger for production also

--- src/CommerceCore.Api/Program.cs
+++ src/CommerceCore.Api/Program.cs
@@ -150,11 +150,10 @@ try
     // ---------- Middleware pipeline ----------
     app.UseGlobalExceptionHandling();
 
-    if (app.Environment.IsDevelopment())
-    {
+   
         app.UseSwagger();
         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CommerceCore API v1"));
-    }
+    
 
     app.UseSerilogRequestLogging();
     app.UseHttpsRedirection();
